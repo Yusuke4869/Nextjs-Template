5dd526e317d91e3e29b18f2d92b95519b71043b7
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-09-02 17:02:50 +0900

    🔨 (Update): use .scss and tsconfig on storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,32 @@
+const path = require("path");
+const tsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal(config) {
+    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, "../src")];
+    config.resolve.plugins = [...(config.resolve.plugins || []), new tsconfigPathsPlugin()];
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            importLoaders: 1, // 1 => postcss-loader
+            modules: {
+              localIdentName: "[local]___[hash:base64:2]",
+            },
+          },
+        },
+        "sass-loader",
+      ],
+    });
+    return config;
   },
 };
