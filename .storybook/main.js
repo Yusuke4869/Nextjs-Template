7ed70c97fceac45477d1270ab3a907fd92dae0c8
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-09-09 22:06:12 +0900

    💄 (Add): storybook addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,17 @@ const tsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-a11y",
+    "@storybook/addon-actions",
+    "@storybook/addon-backgrounds",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-links",
+    "@storybook/addon-measure",
+    "@storybook/addon-outline",
+    "@storybook/addon-viewport",
+  ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
