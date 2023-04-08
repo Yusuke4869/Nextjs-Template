e36e4207355740b982e507ad1cb3d4759947fee5
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2023-04-08 18:42:37 +0900

    💥 support storybook v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,3 @@
-const path = require("path");
-const tsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -8,30 +5,22 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-links",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
-  },
-  webpackFinal(config) {
-    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, "../src")];
-    config.resolve.plugins = [...(config.resolve.plugins || []), new tsconfigPathsPlugin()];
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: [
-        "style-loader",
-        {
-          loader: "css-loader",
-          options: {
-            importLoaders: 1, // 1 => postcss-loader
-            modules: {
-              localIdentName: "[local]___[hash:base64:2]",
-            },
-          },
+    "@storybook/addon-mdx-gfm",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        sass: {
+          // Require your Sass preprocessor here
+          implementation: require("sass"),
         },
-        "sass-loader",
-      ],
-    });
-    return config;
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: true,
   },
 };
