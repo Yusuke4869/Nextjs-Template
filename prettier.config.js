a9b83f3559c46c1a251dd0c888d451608f1aaca5
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2023-08-03 21:02:37 +0900

    🔧 update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,11 @@
-{
+module.exports = {
   "printWidth": 120,
   "tabWidth": 2,
   "useTabs": false,
   "semi": true,
   "trailingComma": "all",
   "endOfLine": "lf",
+  "plugins": [require.resolve("@trivago/prettier-plugin-sort-imports")],
   "importOrder": ["<THIRD_PARTY_MODULES>", "^[./]"],
   "importOrderSeparation": true,
   "importOrderSortSpecifiers": true
