e906719a8f1937901195f2b7b5a2754104e89141
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-10-23 09:20:23 +0900

    chore(deps): remove lint-staged

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
-
-module.exports = {
-  "*.{js,ts,tsx}": [buildEslintCommand, "pnpm format"],
-  "*.css": ["pnpm format"],
-};
