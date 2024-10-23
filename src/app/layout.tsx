d3852e8b328ad08009d98c66377ba8f013a29ee0
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-10-23 09:44:24 +0900

    fix: fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,14 @@
 import { Noto_Sans_JP } from "next/font/google";
-
-import type { FC, ReactNode } from "react";
-
 import "~/styles/globals.css";
 
-type Props = {
-  children: ReactNode;
-};
+import type { FC, PropsWithChildren } from "react";
 
 const notoSansJp = Noto_Sans_JP({
   subsets: ["latin"],
   variable: "--font-noto-sans-jp",
 });
 
-const RootLayout: FC<Props> = ({ children }) => (
+const RootLayout: FC<PropsWithChildren> = ({ children }) => (
   <html lang="ja">
     <body className={notoSansJp.className}>
       <main>{children}</main>
