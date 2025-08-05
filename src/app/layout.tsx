4d47721d509d39dfabfbae1b666b8fd5505a8498
Yusuke <67366917+Yusuke4869@users.noreply.github.com>
2025-08-05 22:00:07 +0900

    chore: add option to enable react-scan (#1129)
    * chore(deps): add react-scan
    
    * feat: embed react-scan
    
    * chore: add npm-scripts called dev:scan that uses react-scan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Noto_Sans_JP } from "next/font/google";
+
 import "../styles/globals.css";
+import { ReactScan } from "./_components/ReactScan";
 
 import type { FC, PropsWithChildren } from "react";
 
@@ -10,6 +12,7 @@ const notoSansJp = Noto_Sans_JP({
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => (
   <html lang="ja">
+    {process.env.NODE_ENV !== "production" && process.env.REACT_SCAN === "true" && <ReactScan />}
     <body className={notoSansJp.className}>
       <main>{children}</main>
     </body>
