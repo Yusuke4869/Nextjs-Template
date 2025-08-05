f2669089ef2d4a247ffca357075e15ee4f2131ec
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2025-08-05 21:44:29 +0900

    fix: use FC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import type { Metadata, NextPage } from "next";
+import type { Metadata } from "next";
+import type { FC } from "react";
 
 export const metadata: Metadata = {
   title: "Hello World!",
 };
 
-const Home: NextPage = () => (
+const Home: FC = () => (
   <div>
     <h1>Hello World</h1>
   </div>
